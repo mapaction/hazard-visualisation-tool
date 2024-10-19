bc1cdc8b60600d29b9a7f79d3bffcf24638527a3
Josh Redmond <jr725@exeter.ac.uk>
2024-10-19 16:10:22 +0100

    added dummy onclick

---
+++ front/scripts/main.js
@@ -0,0 +1,10 @@
+function go_button_onclick(){
+
+
+    var country = document.getElementById("country").value;
+    var admin_level = document.getElementById("admin level").value;
+    var data_type = document.getElementById("data type").value;
+   
+    
+
+}
