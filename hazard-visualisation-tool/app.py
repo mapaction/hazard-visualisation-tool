6212b8d36d0a5d96b5d753d9008e422852ff8c40
Elena Jung <elena.j.jung@gmail.com>
2024-10-19 20:29:18 +0200

    Add file type to endpoint

--- hazard-visualisation-tool/app.py
+++ hazard-visualisation-tool/app.py
@@ -1,11 +1,29 @@
 from flask import Flask
+from pathlib import Path
+
+from markupsafe import Markup
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/<country>/hazard/<hazard>/<admin_level>/")
 def hazard_by_country_and_admin_level(country, hazard, admin_level):
+    file_format = request.args.get("format", type=str)
     print(country)
     print(admin_level)
     print(hazard)
-    return f"<p>Hello, MapAction! You are requesting information for {country} at admin level {admin_level}</p>"
+    print(file_format)
+
+    if file_format == "csv":
+        file_name = "lebanon-draft-01.csv"
+    elif file_format == "geojson":
+        file_name = "lebanon-draft-01.json"
+    else:
+        return f"<p>Hello, MapAction! You are requesting information for {country} at admin level {admin_level}.</p>"
+
+    file_path = Path(__file__).parent.parent / "data" / file_name
+    with open(file_path, encoding="utf8") as f:
+        data = f.read()
+    return Markup(data)
