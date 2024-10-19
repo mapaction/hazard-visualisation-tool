92897c6b0bd9b59ea32844bad897eb5f259878ed
Josh Redmond <jr725@exeter.ac.uk>
2024-10-19 17:41:16 +0100

    added map and tiles

--- front/scripts/main.js
+++ front/scripts/main.js
@@ -23,6 +23,11 @@ function go_button_onclick(){
     document.getElementById('tableContainer').appendChild(table);
 
     var map = L.map('mapContainer').setView([51.505, -0.09], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    
 }
 
 
